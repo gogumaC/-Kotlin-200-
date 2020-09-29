6f50d45a57dd5fa54a3aa2b98f78c348839e69eb
YUBINKIM <kimu2371@naver.com>
2020-09-29 17:23:48 +0900

    test

---
+++ src/14. EX_Comment.kt
@@ -0,0 +1,20 @@
+fun main(args:Array<String>):Unit
+{
+    //Apple을 화면에 출력한다
+    println(/*이부분은 컴파일러가 통째로 무시*/"Apple"/*이
+    부
+    분
+    도
+    무
+    시
+    */
+    )
+
+    val a=15
+    val b=a+5/3.0
+
+    //테스트로 b값이 잘 저장되어 있는지 확인하기 위해 b값 출력
+   // println(b)
+
+    println(a*b)//최종결과ㅏ 출력
+}
---
+++ src/2. EX_Expression.kt
@@ -0,0 +1,5 @@
+fun main(arg:Array<String>):Unit
+{
+    53+62-126
+    println(53+62-126)
+}
